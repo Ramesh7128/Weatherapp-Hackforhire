fef95d4b87d3feccfd28ed8f536f05e2fb126c1a
Ramesh7128 <ramesh7128@gmail.com>
2014-12-07 07:40:21 +0530

    bootstrap

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -7,9 +7,9 @@ from django import forms
 from datetime import datetime, timedelta
 
 class FilterForm(forms.Form):
-    location = forms.CharField(max_length=50,)
-    noofdays = forms.IntegerField(help_text="1-5")
-    condition = forms.CharField(max_length=50,help_text="cloudy,clear,overcast,rain")
+    location = forms.CharField(max_length=50,required=False)
+    noofdays = forms.IntegerField(help_text="range :1-5 days",required=False)
+    condition = forms.CharField(max_length=50,help_text="cloudy,clear,overcast,rain",required=False)
 
 
 def index(request):
@@ -19,38 +19,37 @@ def index(request):
     if request.method == 'POST':
 
         form = FilterForm(request.POST)
+        location = request.POST['location']
+        noofdays = request.POST['noofdays']
+        condition = request.POST['condition']
+        now = datetime.now()
 
-        if form.is_valid():
-            location = request.POST['location']
-            noofdays = request.POST['fromdate']
-            condition = request.POST['condition']
-            now = datetime.now()
+        if location and noofdays and condition:
+            dates = now+timedelta(days=str(noofdays))
+            locationlist = weatherreport.objects.filter(city__icontains=location, condition__icontains=condition,date=(now,dates))
 
-            if location and noofdays and condition:
-                dates = now+timedelta(days=noofdays)
-                locationlist = weatherreport.objects.filter(location=location, condition__icontains=condition,date_range=(now,dates))
+        elif location and noofdays:
+            dates = now+timedelta(days=str(noofdays))
+            locationlist = weatherreport.objects.filter(city__icontains=location, date=(now,dates))
 
-            elif location and noofdays:
-                dates = now+timedelta(days=noofdays)
-                locationlist = weatherreport.objects.filter(location=location, date_range=(now,dates))
+        elif location and condition:
+            locationlist = weatherreport.objects.filter(city__icontains=location, condition__icontains=condition)
 
-            elif location and condition:
-                locationlist = weatherreport.objects.filter(location=location, condition__icontains=condition)
+        elif condition and noofdays:
+            dates = now+timedelta(days=str(noofdays))
+            locationlist = weatherreport.objects.filter(date=(now,dates),condition__icontains=condition)
 
-            elif condition and noofdays:
-                dates = now+timedelta(days=noofdays)
-                locationlist = weatherreport.objects.filter(date_range=(now,dates),condition__icontains=condition)
+        elif condition:
+            locationlist = weatherreport.objects.filter(condition__icontains=condition)
+        elif location:
+            locationlist = weatherreport.objects.filter(city__icontains=location)
 
-            elif condition:
-                locationlist = weatherreport.objects.filter(condition__icontains=condition)
-            elif location:
-                locationlist = weatherreport.objects.filter(location=location)
-
-            else:
-                locationlist = weatherreport.objects.filter(date_range=(now,dates))
+        else:
+            dates = now+timedelta(days=str(noofdays))
+            locationlist = weatherreport.objects.filter(date=(now,dates))
 
 
-            context_dict['locationlist'] = locationlist
+        context_dict['locationlist'] = locationlist
     # if a GET (or any other method) we'll create a blank form
     else:
         form = FilterForm()
